9f92146ed579198951839d9c42897416001cb917
ludik <bakhanetssimon@gmail.com>
2025-12-29 16:47:30 +0700

    Initial Commit

---
+++ KanGastronomia/KanGastronomiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  KanGastronomiaApp.swift
+//  KanGastronomia
+//
+//  Created by Simon Bakhanets on 29.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct KanGastronomiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KanGastronomiaTests/KanGastronomiaTests.swift
@@ -0,0 +1,16 @@
+//
+//  KanGastronomiaTests.swift
+//  KanGastronomiaTests
+//
+//  Created by Simon Bakhanets on 29.12.2025.
+//
+
+import Testing
+
+struct KanGastronomiaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
